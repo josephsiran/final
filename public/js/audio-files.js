21a528bf254e867c8e60c4ad67791cdeea9aad3a
David Porter <dcporter44@users.noreply.github.com>
2015-11-25 16:31:03 -0800

    Add full keyboard

--- public/js/audio-files.js
+++ public/js/audio-files.js
@@ -1,6 +1,6 @@
-LETTERS = ['a','s','d','f','g','h','j','k','l'];
+LETTERS = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
 
-TEMPO = 500;
+TEMPO = 300;
 
 blankAudio = new Audio ();
 
@@ -9,13 +9,15 @@ AUDIOMAP = {
 };
 
 var createAudio = function(letter) {
-  dynamicString = letter + "Audio1 = new Audio ();" + letter + "Audio1.src = 'images/audio/" + letter + "_audio.mp3';";
+  dynamicString = letter + "Audio1 = new Audio ();" + letter +
+    "Audio1.src = 'images/audio/" + letter + "_audio.mp3';";
   eval(dynamicString);
   (eval(letter + "Audio1")).addEventListener('loadedmetadata', function () {
     AUDIOMAP[letter] = [eval(letter + "Audio1")];
-    var count = (eval(letter + "Audio1")).duration / (TEMPO / 1000) + 1;
+    var count = (eval(letter + "Audio1")).duration / (TEMPO / 1000) + 2;
     for (var i = 2; i <= count; i++) {
-      dynamicString = letter + "Audio" + i + "= new Audio ();" + letter + "Audio" + i + ".src = 'images/audio/" + letter + "_audio.mp3';";
+      dynamicString = letter + "Audio" + i + "= new Audio ();" + letter +
+        "Audio" + i + ".src = 'images/audio/" + letter + "_audio.mp3';";
       eval(dynamicString);
       AUDIOMAP[letter].push(eval(letter + "Audio" + i));
     }
