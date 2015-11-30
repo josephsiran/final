318e6e9e3e7ca6e7338599111423f87bf2ffbe9a
David Porter <dcporter44@users.noreply.github.com>
2015-11-29 17:33:16 -0800

    Add footer

---
+++ public/js/components/footer.js.jsx
@@ -0,0 +1,10 @@
+var Footer = React.createClass({
+  render: function () {
+    return (
+      <div className="footer-container">
+        <p>Created by <a className="footer-link" href="http://www.linkedin.com/in/dcporter7">David Porter</a>.</p>
+        <p>Inspired by Kyle Stetz's <a className="footer-link" href="http://www.typedrummer.com">TypeDrummer</a>.</p>
+      </div>
+    )
+  }
+})
--- public/js/routes.js.jsx
+++ public/js/routes.js.jsx
@@ -9,6 +9,7 @@ $(function(){
       return (
           <div>
             {this.props.children}
+            <Footer/>
           </div>
       );
     }
